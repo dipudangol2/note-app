ec3a67a5ebffd6f111676ff4343555ec94798ad0
dipudangol2 <dipudangol2@gmail.com>
2024-10-17 19:02:46 +0545

    Implement Dragging Cards and auto resize of notes

--- note-app/src/components/NoteCard.jsx
+++ note-app/src/components/NoteCard.jsx
@@ -1,23 +1,76 @@
+import { setNewOffset , autoGrow} from "../utils";
+import Trash from "../icons/Trash";
+import { useRef, useEffect, useState } from "react";
 const NoteCard = ({ note }) => {
-    let position = JSON.parse(note.position);
+    let [position, setPosition] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
+    let mouseStartPos = { x: 0, y: 0 };
+    const cardRef = useRef(null);
+    const textAreaRef = useRef(null);
+
+
+
+    const mouseDown = (e) => {
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    };
+
+    const mouseMove = (e) => {
+        const mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+
+        setPosition(newPosition);
+    };
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp)
+    }
+
+    useEffect(() => {
+        autoGrow(textAreaRef);
+    }, []);
+
     return (
         <div
             className="card"
+            ref={cardRef}
             style={{
-                backgroundColor: colors.colorBody
-            }}>
-            <div className="card-header"
-                style={{ backgroundColor: colors.colorHeader }}>
-                        hello
+                backgroundColor: colors.colorBody,
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+            }}
+        >
+            <div
+                onMouseDown={mouseDown}
+                className="card-header"
+                style={{
+                    backgroundColor: colors.colorHeader,
+                }}
+            >
+                <Trash />
             </div>
 
             <div className="card-body">
-                <textarea style={{ color: colors.colorText }}
-                    defaultValue={body}></textarea>
+                <textarea
+                    ref={textAreaRef}
+                    onInput={() => {
+                        autoGrow(textAreaRef);
+                    }}
+                    style={{ color: colors.colorText }}
+                    defaultValue={body}
+                ></textarea>
             </div>
-
         </div>
     );
 };
